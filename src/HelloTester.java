d0501f5d1cdc80fd60c9a61343bce6ceb1d08b57
Gabriel Jones <gabrieljones@Gabriels-MacBook-Pro.local>
2017-02-27 12:44:13 -0700

    Finish exercise 2 for 30

--- src/HelloTester.java
+++ src/HelloTester.java
@@ -4,7 +4,8 @@ class HelloObject
   void speak(String greeting)
   {
 	for (int i = 0; i < greeting.length(); i++) {
-    	System.out.println(greeting);
+    	System.out.println(greeting + ", good morning!");
+    	System.out.println(greeting + ", good evening!");
 	}
   }
 }
